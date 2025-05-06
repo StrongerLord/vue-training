bd24cf2f6dbaca4af2caf0f62f6453eae9eb332a
Jesús Adán Salazar Campos <adan10104334@gmail.com>
2025-05-05 20:17:05 -0600

    style: update layout and styling of TheWelcome component for improved responsiveness

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -23,18 +23,22 @@ const fetchUrl = async () => {
 }
 </script>
 <template>
-  <div class="flex flex-col lg:w-[20%] p-10 text-2xl space-y-3 w-[80%] items-center">
-    <h1 class="">Welcome to the app!</h1>
-    <p>This is a simple Vue.js application.</p>
-    <p>This is a counter app but using vue!</p>
-    <button
-      class="bg-blue-950 text-white p-4 rounded-2xl hover:bg-blue-800 transition"
-      @click="increment"
-    >
-      Click me! {{ count }}
-    </button>
+  <div
+    class="p-[10%] text-2xl w-full flex flex-col xl:flex-row items-center xl:justify-center xl:space-x-32"
+  >
+    <div class="flex flex-col items-center space-y-3 mb-3 xl:w-[30%]">
+      <h1 class="">Welcome to the app!</h1>
+      <p>This is a simple Vue.js application.</p>
+      <p>This is a counter app but using vue!</p>
+      <button
+        class="bg-blue-950 text-white p-4 rounded-2xl hover:bg-blue-800 transition"
+        @click="increment"
+      >
+        Click me! {{ count }}
+      </button>
+    </div>
     <div
-      class="rounded-3xl overflow-hidden transition-opacity aspect-square w-full bg-gray-200"
+      class="rounded-3xl overflow-hidden transition-opacity aspect-square w-full xl:w-[55%] bg-gray-200"
       :class="{
         'bg-gray-400': !url,
       }"
